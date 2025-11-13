138dd1a39ba2862b5ab743fa53568396bebf2783
ahronnnnn <ahronclaro01@gmail.com>
2025-11-13 18:33:37 +0800

    just commit

---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo; // <-- Import this
+
+class Product extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'category_id'
+    ];
+
+    /**
+     * Get the category that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+
+     public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
